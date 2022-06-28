a6c4dd8bb772b200c1ee4012b784e2de9068ffe1
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2022-06-28 23:18:07 +0200

    Removed unnecessary imports and changed password input placeholder on login page

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import {Spinner, LockBody, ReleaseBody, Picture} from './styles/loading'
+import { Spinner, LockBody, ReleaseBody, Picture } from "./styles/loading";
 
-export default function Loading({src, ...restProps}){
-    return (
-        <Spinner>
-            <LockBody />
-            <Picture src={`/images/users/${src}.png`} />
-        </Spinner>
-    )
+export default function Loading({ src, ...restProps }) {
+  return (
+    <Spinner>
+      <LockBody />
+      <Picture src={`/images/users/${src}.png`} />
+    </Spinner>
+  );
 }
 
-Loading.ReleaseBody = function LoadingReleaseBody(){
-    return <ReleaseBody />;
-}
+Loading.ReleaseBody = function LoadingReleaseBody() {
+  return <ReleaseBody />;
+};

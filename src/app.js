a60af7e01d89aa4606b1984a6d60bf8c87a4019a
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2022-06-28 23:10:52 +0200

    updated all npm packages

--- src/app.js
+++ src/app.js
@@ -1,39 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import * as ROUTES from './constants/routes.js';
-import { Home, Browse, Signin, Signup } from './pages'
-import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
-import {useAuthListener} from './hooks';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes.js";
+import { Home, Browse, Signin, Signup } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
 
 export default function App() {
+  const { user } = useAuthListener();
 
-    const {user} = useAuthListener();
-    
-    return (
-        <Router>
-            <IsUserRedirect
-                user={user}
-                loggedInPath={ROUTES.BROWSE}
-                path={ROUTES.SIGN_IN} 
-            >
-                <Signin />
-            </IsUserRedirect>
-
-            <IsUserRedirect
-                user={user}
-                loggedInPath={ROUTES.BROWSE}
-                path={ROUTES.SIGN_UP}  
-            >
-                <Signup />
-            </IsUserRedirect>
-
-            <ProtectedRoute user={user} path={ROUTES.BROWSE} >
-                <Browse />
-            </ProtectedRoute>
-
-            <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
-                <Home />
-            </IsUserRedirect>
-        </Router>
-    )
+  return (
+    <Router>
+      <Routes>
+        <Route
+          element={<IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} />}
+        >
+          <Route path={ROUTES.SIGN_IN} element={<Signin />} />
+          <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+          <Route path={ROUTES.HOME} element={<Home />} />
+        </Route>
+        <Route element={<ProtectedRoute user={user} />}>
+          <Route path={ROUTES.BROWSE} element={<Browse />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import { render } from 'react-dom';
-import 'normalize.css';
-import App from './app';
-import { GlobalStyles } from './global-styles';
-import { firebase } from './lib/firebase.prod';
-import { FirebaseContext } from './context/firebase';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "normalize.css";
+import App from "./app";
+import { GlobalStyles } from "./global-styles";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
-render(
-    <>
-        <FirebaseContext.Provider value={{ firebase }}>
-            <GlobalStyles />
-            <App />
-        </FirebaseContext.Provider>
-    </>
-    , document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <FirebaseContext.Provider value={{ firebase }}>
+    <GlobalStyles />
+    <App />
+  </FirebaseContext.Provider>
 );
